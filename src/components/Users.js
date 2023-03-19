c25f0645182493d1a677d0dadaf3b0a1f00a355a
vinit-churi <vinitchuri@hotmail.com>
2023-03-19 13:41:29 +0530

    test

--- src/components/Users.js
+++ src/components/Users.js
@@ -4,7 +4,7 @@ function Users({ users, dispatch, setBalance, balance }) {
     console.log(users);
 
     function handleFuel() {
-        // dispatch({ type: "fuel", setBalance, balance });
+        dispatch({ type: "fuel", setBalance, balance });
     }
     return (
         <div>
@@ -13,7 +13,7 @@ function Users({ users, dispatch, setBalance, balance }) {
             </button>
             {users.map((user) => {
                 return (
-                    <div key={user.id} class="user">
+                    <div key={user.id} className="user">
                         {user.name}
                     </div>
                 );
--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,19 @@
 import { v4 as uuidv4 } from "uuid";
 
-const reducer = (state, action) => {};
+const reducer = (state, action) => {
+    if (action.type === "add") {
+        action.setBalance((balance) => {
+            return balance + 10;
+        });
+        return [...state, { name: action.name, id: uuidv4() }];
+    } else if (action.type === "fuel") {
+        if (action.balance >= 50) {
+            action.setBalance((balance) => balance - 50);
+        }
+        return state;
+    } else {
+        return state;
+    }
+};
 
 export default reducer;
